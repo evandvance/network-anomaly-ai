864c666f809f96b557f8e78cb532088d39d4d8e6
Evan <evandvance@gmail.com>
2024-11-19 12:05:22 -0600

    Add save and load functions

--- helpers.py
+++ helpers.py
@@ -1,8 +1,26 @@
 from sklearn.base import BaseEstimator
-from typing import Callable, Dict
+from typing import Callable
+from datetime import datetime, timezone
 import numpy.typing as npt
+import pickle
+import os
 
 def trainer_factory(model: BaseEstimator, X: npt.ArrayLike, y: npt.ArrayLike, supervised:bool=False) -> Callable:
     def trainer(**kwargs) -> BaseEstimator:
-        return model(**kwargs).fit(X, y) if supervised else model(**kwargs).fit(X)
+        mdl = model(**kwargs).fit(X, y) if supervised else model(**kwargs).fit(X)
+        save_model(mdl)
+        return mdl
+
     return trainer
+
+def save_model(model:BaseEstimator) -> str:
+    time = str(datetime.now(timezone.utc)).replace(" ","")
+    path = f"{os.getcwd()}/models/{model.__class__.__name__}_{time}.mdl"
+
+    with open(path,'wb') as f:
+        pickle.dump(model,f)
+    return path
+
+def load_model(path:str) -> BaseEstimator:
+    with open(path, 'rb') as f:
+        return pickle.load(f)
