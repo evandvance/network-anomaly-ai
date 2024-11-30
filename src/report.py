2a6eb950cb24843381d230be7d7ce32363c9d06b
Evan <evandvance@gmail.com>
2024-11-30 10:28:13 -0600

    Demo Prep

--- src/report.py
+++ src/report.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 def generate_report(predictions: pd.DataFrame, filename:str):
     print(f"Saving results to {filename }.csv")
-    predictions.to_csv(filename + ".pcap_report.csv")
+    predictions.to_csv(filename + ".report.csv")
